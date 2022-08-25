12b29883735d6b7ec516c4021d7dc981cf3dc3e7
girordo <tsgiroldo@gmail.com>
2022-08-25 02:16:50 -0300

    🔧 | configuring the configuration again

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import styles from './styles'
 
 const App = () => {
-  return(
-    <div>
-      App
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <section className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <section className={`${styles.boxWidth}`}>Navbar</section>
+      </section>
+
+      <section className={`bg-primary ${styles.flexStart}`}>
+        <section className={`${styles.boxWidth}`}>Hero</section>
+      </section>
+
+      <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <section className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonials Clients
+        </section>
+      </section>
     </div>
   )
 }
 
-export default App;
+export default App
